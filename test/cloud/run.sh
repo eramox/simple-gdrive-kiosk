89816ba679d29fe6e01c75720506bb24760341ba
Franck LENORMAND <franck.lenormand.fr@gmail.com>
2024-01-16 22:56:21 +0100

    RW run script

--- test/cloud/run.sh
+++ test/cloud/run.sh
@@ -9,20 +9,29 @@ KIOSK_DIR="$(realpath "${ROOT_DIR}/cb_kiosk" )"
 
 # Note the version file must be edited to point to a ppt file
 VERSION_FILE="https://docs.google.com/document/d/11hGse_OyqGQiy3qAaDMSHnrFX0_YyG2o/edit?usp=drive_link&ouid=104564737776631583414&rtpof=true&sd=true"
-VENV="${VENV:-}"
 
-if [ -n "${VENV}" ];then
-	. "${VENV}/bin/activate"
-else
-	rm -Rf venv
-	python3 -m venv venv
-	. ./venv/bin/activate
-	pip install -r "${ROOT_DIR}/conf/requirements.txt"
+echo "Running from dir ${PWD}"
+
+# Set the path to the venv if it is not defined
+if [ ! -n "${VENV}" ];then
+	VENV=venv
 fi
+
+# Create the venv if it does not exists
+if [ ! -d "${VENV}" ];then
+	python3 -m venv "${VENV}"
+fi
+
+# Start the venv
+. "${VENV}/bin/activate"
+
+# Update the venv
+pip install -r "${ROOT_DIR}/conf/requirements.txt"
 
 # Echo print env
 set
 
 env
 
+# Start the main script
 python3 "${KIOSK_DIR}/main.py" "${VERSION_FILE}"
